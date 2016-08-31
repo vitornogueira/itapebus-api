eb7cabc9ee143cd243ff1fced66ea12f739bd2d2
Vitor Nogueira <vnweb.contato@gmail.com>
2016-08-31 19:56:44 -0300

    Add L 01

--- config/sheets.js
+++ config/sheets.js
@@ -1,4 +1,10 @@
 module.exports = {
+  'linha-01': {
+    description: 'xl215',
+    subtitles: 'xl220',
+    breakLineClasses: ['xl220', 'xl210'],
+    hourClasses: ['xl221', 'xl225', 'xl234', 'xl235']
+  },
   'linha-02': {
     description: 'xl150',
     subtitles: 'xl155',
